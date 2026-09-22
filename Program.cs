8eed21bd5bf5e3486e24959761dce8e5ef60e87b
Leonardo Alvarez <leogabrialne50@gmail.com>
2026-09-22 09:25:27 -0500

    nota alta y baja

--- Program.cs
+++ Program.cs
@@ -12,3 +12,22 @@ for (int i = 0; i < notas.Length; i++)
 }
 double promedio= (double)suma/notas.Length; //calcular el promedioo
 Console.WriteLine($"Promedio: {promedio:F2}"); //Mostrar el promedio con dos decimales
+
+//Determinar la nota mas alta y la mas baja
+int notaMasAlta = notas[0];
+int notaMasBaja = notas[0];
+for(int i = 1; i<notas.Length; i++)
+{
+    if(notas[i]> notaMasAlta)
+    {
+        notaMasAlta=notas[i]; //Actualizar la nota mas alta si se encuentra una mayor
+    }
+    if(notas[1] < notaMasBaja)
+    {
+        notaMasBaja=notas[1]; //Actualizar la nota mas baja si se encuentra una menor
+
+    }
+
+}
+Console.WriteLine($"Nota mas alta: {notaMasAlta}");
+Console.WriteLine($"Nota mas baja: {notaMasBaja}")
